74e8434c018c716871134b6de7f1b6fe07d76689
Amel Gannoun <amel.gannoun@etudiant-enit.utm.tn>
2024-09-08 14:52:31 +0100

    fix resources names

--- infra/route_table.tf
+++ infra/route_table.tf
@@ -1,9 +1,9 @@
 resource "aws_route_table" "main_route_table" {
-  vpc_id = aws_vpc.main_vpc.id
+  vpc_id = aws_vpc.main.id
 }
 
 resource "aws_route" "main_route" {
   route_table_id     = aws_route_table.main_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id         = aws_internet_gateway.main_ig.id
+  gateway_id         = aws_internet_gateway.main.id
 }
