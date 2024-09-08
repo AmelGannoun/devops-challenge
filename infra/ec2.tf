d0f76e70d6a34395a76ac966ef6e2d6a0f188d8d
Amel Gannoun <amel.gannoun@etudiant-enit.utm.tn>
2024-09-08 15:10:57 +0100

    fix syntax

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "bird-instance" {
-  image_id           = "ami-04a92520784b93e73"  
+  ami           = "ami-04a92520784b93e73"  
   instance_type      = "t2.micro"
   key_name           = "bird-key-pair-app"
   subnet_id    = aws_subnet.public.id
