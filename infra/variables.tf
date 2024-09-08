f5a6531c80d1e9994b8a228e4e946a242195bb41
Amel Gannoun <amel.gannoun@etudiant-enit.utm.tn>
2024-09-08 14:07:09 +0100

    define the internet gateway

---
+++ infra/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "The AWS region to deploy into"
+  default     = "eu-west-3"
+}
+
+variable "aws_profile" {
+  description = "The AWS CLI profile to use"
+  default     = "default"
+}
